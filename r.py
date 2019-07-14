33f9b37f64f44c122ec7d7f1d66421dce2a8bc7a
Kelvin <xile1973@163.com>
2019-07-14 20:14:13 +0800

    calc avergy

--- r.py
+++ r.py
@@ -5,4 +5,11 @@ with open ('food.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
 
-print(len(data))
+print('清单记录共有', len(data),'条')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print('字符长度是', sum_len)
+
+print('留言平均长度是', sum_len/len(data))
