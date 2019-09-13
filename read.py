28d5f73e6fdabdbfb1cf5abf2f26cfe707a4505b
xile1973 <52164425+xile1973@users.noreply.github.com>
2019-09-13 21:52:43 +0800

    Update read.py

--- read.py
+++ read.py
@@ -11,13 +11,13 @@ print('档案读取完毕，总共有', len(data), '笔资料')
 print(data[0])
 
 wc = {} #word_count
-for d in data:# data 是一个清单，里面装着100万笔留言
-	words = d.split()# 把留言用空格分割
+for d in data:# data是一个清单，里面装着100万笔留言，而d是全部留言中的一个。
+	words = d.split()# 把留言用空格分割，split()使用的是该函数的预设值，使用空格来分割句子中的单词，且不会把连续的空格当做单词。
 	for word in words:# 把分割后的每一个字放到word中
 		if word in wc:#如果这个字出现在字典里
 			wc[word] += 1
 		else:
-			wc[word] = 1 #新增新的key进入字典
+			wc[word] = 1 #新增新的key进入字典，同时给value赋值为1
 for word in wc:
 	if wc[word] >1000000:
 		print(word, wc[word])
